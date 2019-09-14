3c7aeaf5e07d2e34f0c4eb0e9eb5bd942b166b46
Kris <Kristopher.Kath@gmail.com>
2019-09-13 19:35:46 -0700

    Update main.cpp

--- MyGameEngine/main.cpp
+++ MyGameEngine/main.cpp
@@ -5,13 +5,38 @@
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 480;
 
+
+//Starts up SDL and creates window
+bool init() 
+{
+	//Initialization flag
+	bool success = true;
+
+	//Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not be initialized! SDL_Error: %s\n", SDL_Error());
+		success = false;
+	}
+};
+
+//Loads media
+bool loadMedia();
+
+//Frees media and shuts down SDL
+void close();
+
+
 int main(int argc, char* args[])
 {
 	//The window we'll be rendering to
-	SDL_Window* window = NULL;
+	SDL_Window* gWindow = NULL;
 
 	//The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
+	SDL_Surface* gScreenSurface = NULL;
+
+	//The image we load on the screen
+	SDL_Surface* gHelloWorld = NULL;
 
 	//Initialize SDL
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
@@ -21,21 +46,21 @@ int main(int argc, char* args[])
 	else
 	{
 		//Create window
-		window = SDL_CreateWindow("Kris's Game Engine", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == NULL)
+		gWindow = SDL_CreateWindow("Kris's Game Engine", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (gWindow == NULL)
 		{
 			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
 		}
 		else
 		{
 			//Get window surface
-			screenSurface = SDL_GetWindowSurface(window);
+			gScreenSurface = SDL_GetWindowSurface(gWindow);
 
 			//Fill the surface white
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+			SDL_FillRect(gScreenSurface, NULL, SDL_MapRGB(gScreenSurface->format, 0xFF, 0xFF, 0xFF));
 
 			//Update the surface
-			SDL_UpdateWindowSurface(window);
+			SDL_UpdateWindowSurface(gWindow);
 
 			//Wait two seconds
 			SDL_Delay(2000);
@@ -43,7 +68,7 @@ int main(int argc, char* args[])
 	}
 
 	//Destroy window
-	SDL_DestroyWindow(window);
+	SDL_DestroyWindow(gWindow);
 
 	//Quit SDL subsystems
 	SDL_Quit();
